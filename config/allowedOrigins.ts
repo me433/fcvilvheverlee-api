9a29e5b0232479bffd9d4c2189cd4b25ca2ae466
Milo Wera <milowera@Milos-MacBook-Air.local>
2024-11-11 17:26:56 +0100

    reworked for Firestore

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://versatile-gist-439621-k5.ew.r.appspot.com/'
 ];
 
 module.exports = allowedOrigins;
